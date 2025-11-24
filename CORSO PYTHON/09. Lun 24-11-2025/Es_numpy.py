a23e15e1655b79094b5b22550f2662d5cf7e7d42
Emanuele <emanueleanzellotti@gmail.com>
2025-11-24 16:10:35 +0100

    up

--- CORSO PYTHON/09. Lun 24-11-2025/Es_numpy.py
+++ CORSO PYTHON/09. Lun 24-11-2025/Es_numpy.py
@@ -95,12 +95,12 @@ print(arr[2:7]) # Output: [2 3 4 5 6]
 print(arr[1:8:2]) # Output: [1 3 5 7]
 
 # Omettere start e stop
-print(arr[:5]) # Output: [0 1 2 3 4]
-print(arr[5:]) # Output: [5 6 7 8 9]
+print(arr[:5]) # Output: [0 1 2 3 4] -> i primi 5
+print(arr[5:]) # Output: [5 6 7 8 9] -> da 5 in poi
 
 # Utilizzare indici negativi
-print(arr[-5:]) # Output: [5 6 7 8 9]
-print(arr[:-5]) # Output: [0 1 2 3 4]
+print(arr[-5:]) # Output: [5 6 7 8 9] -> gli ultimi 5
+print(arr[:-5]) # Output: [0 1 2 3 4] -> i primi 5
 
 """
 
